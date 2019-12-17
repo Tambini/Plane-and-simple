fc079bef15cb0def0962bad8574da8d8a39b553e
Tambini <tambini.c@gmail.com>
2019-12-17 17:50:11 -0500

    updated my api

--- plane-and-simple/src/services/apiHelper.js
+++ plane-and-simple/src/services/apiHelper.js
@@ -3,25 +3,28 @@ import axios from 'axios';
 const url = "https://www.triposo.com/api/20190906/location.json?";
 const accountID = "ULDPYY8A"
 const apiToken = "7jwucw1j1gjrqjg8j67gbnu57i3zpz0u"
-const input = "Amsterdam"
+const city = ""
 
 
 export const fetchData = async () => {
-  const data = await axios.get(`${url}id=${input}&account=${accountID}&token=${apiToken}&fields=all`);
+  const data = await axios.get(`${url}id=${city}&account=${accountID}&token=${apiToken}&fields=all`);
   return data
 }
 
 
 export const allCities = async () => {
-  //
-  const cities = ["London", "Madrid", "Paris", "Dubai", "Amsterdam", "Milan", "Sydney", " Lima", "Rio_de_Janeiro", "New_York"]
+
+  const cities = ["London", "Madrid", "Paris", "Dubai", "Amsterdam", "Sydney", "Lima", "Rio_de_Janeiro", "Barcelona", "Milan", "Melbourne"]
 
   const cityUrls = cities.map((city) => (`${url}id=${city}&account=${accountID}&token=${apiToken}&fields=all`)
 
   )
 
-  console.log(cityUrls)
+  // console.log(cityUrls)
+
+
   const cityPromises = cityUrls.map(cityUrl => axios.get(cityUrl))
+
 
   const cityData = await Promise.all(cityPromises)
 
