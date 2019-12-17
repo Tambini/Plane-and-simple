dd6765caf0fb59fb2a3f0d1720ffe6ea3c6a984d
Tambini <tambini.c@gmail.com>
2019-12-17 09:12:54 -0500

    updated main app.js file

--- plane-and-simple/src/App.js
+++ plane-and-simple/src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 import ProfilePage from './components/ProfilePage';
+import LandingPage from './components/LandingPage';
+import { allCities } from "./services/apiHelper"
 
 
 class App extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount = async () => {
+    const response = await allCities()
+    console.log(response)
   }
 
 
-  
+
   render() {
 
     return (
       <div className="App">
         <ProfilePage />
+        <LandingPage />
       </div>
     );
   }
