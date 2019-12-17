32fee85e00146bb8427cf18456976263afb2f9fe
Tambini <tambini.c@gmail.com>
2019-12-17 17:48:40 -0500

    refactored landing page and changed from class state to a function

--- plane-and-simple/src/components/LandingPage.js
+++ plane-and-simple/src/components/LandingPage.js
@@ -1,43 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { fetchData, allCities } from '../services/apiHelper';
+import { Link } from 'react-router-dom';
 
-class Landing extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      cities: [],
-      image: "",
-    }
-  }
-
-  componentDidMount = async () => {
-    const citiesData = await allCities()
-    console.log(citiesData)
-
-    this.setState({
-      cities: citiesData,
-    })
-  }
-
-
-  render() {
-    // console.log(this.state.cities.map((city, index) => {
-    //   return city.data
-    // }))
-    return (
-      this.state.cities.length > 0 ?
-        <div className="profile-container">
-          {this.state.cities.map((city, index) => {
+export default function Landing(props) {
+  return (
+    <div>
+      {props.cities &&
+        <div className="landing-container">
+          {props.cities.map((city, index) => {
             return (
-              <img key={index} src={city.data.results[0].images[0].sizes.original.url} />
-              // <p>{city.data.results[0].images[0]}</p>
+              <Link to={`/profile/${city.data.results[0].id}`}>
+                <img className="landing-page-images" key={index} src={city.data.results[0].images[0].sizes.original.url} />
+              </Link>
             )
           })}
-        </div> :
-        <p>NOT WORKING</p>
-    )
-  }
+        </div>}
+    </div>
+  )
+
 }
 
-export default Landing;
