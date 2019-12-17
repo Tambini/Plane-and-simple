bb474e01262475775217219b817b69e97e802f10
Tambini <tambini.c@gmail.com>
2019-12-17 17:51:06 -0500

    in progress of updating profile component

--- plane-and-simple/src/components/ProfilePage.js
+++ plane-and-simple/src/components/ProfilePage.js
@@ -1,50 +1,43 @@
 import React, { Component } from "react";
 
-import { fetchData } from '../services/apiHelper';
+import { allCities } from '../services/apiHelper';
 
-class ProfilePage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      image: "",
-      snippit: "",
-      population: "",
-  
+export default function ProfilePage(props) {
+  // componentDidMount = async () => {
+  //   // const response = await fetchData()
+  //   // console.log(response.data);
+  //   // let cityImage = response.;
+  //   // let citySnippit = response.data.results[0].intro;
+  //   // let cityPopulation = response.data.results[0].properties[0].value;
+  //   // console.log(cityImage);
+  //   // console.log(citySnippit);
+  //   // console.log(cityPopulation);
+  //   // // console.log(response.data.results)
 
-    }
+  //   // this.setState({
+  //   //   image: cityImage,
+  //   //   snippit: citySnippit,
+  //   //   population: cityPopulation,
+  //   // })
 
-  }
+  // }
 
-  componentDidMount = async () => {
-    const response = await fetchData()
-    let cityImage = response.data.results[0].images[0].sizes.original.url;
-    let citySnippit = response.data.results[0].intro;
-    let cityPopulation = response.data.results[0].properties[0].value;
-    console.log(cityImage);
-    console.log(citySnippit);
-    console.log(cityPopulation);
-    console.log(response.data.results)
+  console.log(props)
+  return (
+    <div className="profile-container">
+      {props &&
+        <img src={props.city.images[0].sizes.original.url} alt="city-image" />
+      }
+      {/* <h1> {this.state.snippit}</h1>
+      <h2>Population = {this.state.population}</h2> */}
 
-    this.setState({
-      image: cityImage,
-      snippit: citySnippit,
-      population: cityPopulation,
-    })
+    </div>
 
-  }
-
-  render() {
-    return (
-      <div className="profile-container">
-        <img src={this.state.image} alt="city-image" />
-        <h1> {this.state.snippit}</h1>
-        <h2>Population = {this.state.population}</h2>
-      </div>
-    )
-  }
+  )
 }
 
-export default ProfilePage
+
+
 
 
 
