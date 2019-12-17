95bd99e39619179ebc5ce90c22824fc14acb1c7e
Tambini <tambini.c@gmail.com>
2019-12-17 17:47:34 -0500

    refactored where my api call is

--- plane-and-simple/src/App.js
+++ plane-and-simple/src/App.js
@@ -10,30 +10,48 @@ import Footer from './components/Footer'
 //api calls
 import { allCities } from "./services/apiHelper"
 
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-
+      cities: [],
+      city: "",
+      apiDataLoaded: false
     }
+
   }
 
   componentDidMount = async () => {
-    const response = await allCities()
-    console.log(response)
+    const citiesData = await allCities()
+    console.log(citiesData)
+
+    this.setState({
+      cities: citiesData,
+      apiDataLoaded: true
+    })
+  }
+
+  displayCity = () => {
+
   }
 
 
-
   render() {
-
+   
     return (
       <div className="App">
         <Nav />
-        <ProfilePage />
-        <LandingPage />
+        <main>
+          <Route exact path="/" render={(props) =>
+            <LandingPage cities={this.state.cities} />} />
+          <Route path="/profile/:cityId" render={(props) => {
+            return this.state.apiDataLoaded ?
+              <ProfilePage city={this.state.cities[0].data.results[0]} /> : null
+          }} />
+        </main>
         <Footer />
       </div>
     );
