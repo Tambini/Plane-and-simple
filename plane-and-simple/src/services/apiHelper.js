5085d83751d89d54f29da6ee8098db6f8bf4af10
Tambini <tambini.c@gmail.com>
2019-12-17 09:06:38 -0500

    added new api call for landing page

--- plane-and-simple/src/services/apiHelper.js
+++ plane-and-simple/src/services/apiHelper.js
@@ -3,19 +3,31 @@ import axios from 'axios';
 const url = "https://www.triposo.com/api/20190906/location.json?";
 const accountID = "ULDPYY8A"
 const apiToken = "7jwucw1j1gjrqjg8j67gbnu57i3zpz0u"
-const input = "Boston"
+const input = "Amsterdam"
 
 
-  export const fetchData = async () => {
-    const data = await axios.get(`${url}id=${input}&account=${accountID}&token=${apiToken}&fields=all`);
-    return (data)
-  }
+export const fetchData = async () => {
+  const data = await axios.get(`${url}id=${input}&account=${accountID}&token=${apiToken}&fields=all`);
+  return data
+}
 
-  // const cities =["London", "Madrid", "Paris", "Dubai", "Amsterdam", "Milan", "Sydney", " Lima", ]
 
-  // export const allCities = async () => {
-  //   const data = await axios.get(`${url}id=${input}&account=${accountID}&token=${apiToken}&fields=all`);
-  //   return (data)
-  // }
+export const allCities = async () => {
+  //
+  const cities = ["London", "Madrid", "Paris", "Dubai", "Amsterdam", "Milan", "Sydney", " Lima", "Rio_de_Janeiro", "New_York"]
+
+  const cityUrls = cities.map((city) => (`${url}id=${city}&account=${accountID}&token=${apiToken}&fields=all`)
+
+  )
+
+  console.log(cityUrls)
+  const cityPromises = cityUrls.map(cityUrl => axios.get(cityUrl))
+
+  const cityData = await Promise.all(cityPromises)
+
+
+
+  return (cityData)
+}
 
 
