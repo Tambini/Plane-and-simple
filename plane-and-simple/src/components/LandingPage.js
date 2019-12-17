0b4c12654319f5a0ee3acbe978c46421d54c1d5d
Tambini <tambini.c@gmail.com>
2019-12-17 09:08:54 -0500

    added landing Page code to pull in new array of all cities

--- plane-and-simple/src/components/LandingPage.js
+++ plane-and-simple/src/components/LandingPage.js
@@ -1,39 +1,30 @@
 import React, { Component } from "react";
 
-import { fetchData } from '../services/apiHelper';
+import { fetchData, allCities } from '../services/apiHelper';
 
 class Landing extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input = []  
+      cities: [],  
       image: "",
-
     }
-
   }
 
   componentDidMount = async () => {
-    const response = await fetchData()
-    let cityImage = response.data.results[0].images[0].sizes.original.url;
-  
-    console.log(cityImage);
-  
-    console.log(response.data.results)
-
-    this.setState({
-      image: cityImage,
-    
-    })
-
+    const response = await allCities()
+    console.log(response)
   }
+
+  DisplayCities= () => {
+    
+  }
+
 
   render() {
     return (
       <div className="profile-container">
         <img src={this.state.image} alt="city-image" />
-        <h1> {this.state.snippit}</h1>
-        <h2>Population = {this.state.population}</h2>
       </div>
     )
   }
