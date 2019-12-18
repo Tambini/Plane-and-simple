eae8d97e009bad2c334b876d4f22d7ff7973b6c0
Tambini <tambini.c@gmail.com>
2019-12-18 12:00:05 -0500

    had to delete the previous way of pulling in the images for the profile page becuase I would have had to have different components for each profile page and that would not work

--- plane-and-simple/src/App.js
+++ plane-and-simple/src/App.js
@@ -26,57 +26,34 @@ class App extends Component {
   }
 
 
-  componentDidMount = async () => { 
+  componentDidMount = async () => {
     const citiesData = await allCities()
-    const londonCityImage = citiesData[0].data.results[0].images[1].sizes.original.url
-    const madridCityImage = citiesData[1].data.results[0].images[0].sizes.original.url
-    const parisCityImage = citiesData[2].data.results[0].images[0].sizes.original.url
-    const dubaiCityImage = citiesData[3].data.results[0].images[0].sizes.original.url
-    const amsterdamCityImage = citiesData[4].data.results[0].images[0].sizes.original.url
-    const sydneyCityImage = citiesData[5].data.results[0].images[0].sizes.original.url
-    const limaCityImage = citiesData[6].data.results[0].images[0].sizes.original.url
-    const rioCityImage = citiesData[7].data.results[0].images[0].sizes.original.url
-    const barcelonaCityImage = citiesData[8].data.results[0].images[0].sizes.original.url
-    const milanCityImage = citiesData[9].data.results[0].images[0].sizes.original.url
-    const melbourneCityImage = citiesData[10].data.results[0].images[0].sizes.original.url
-    const athensCityImage = citiesData[10].data.results[0].images[0].sizes.original.url
+    console.log(citiesData)
+    const cityImage = citiesData[0].data.results[0].images[1].sizes.original.url
   
-      this.setState({
-        cities: citiesData,
-        londonCityImage: londonCityImage,
-        madridCityImage: madridCityImage,
-        parisCityImage: parisCityImage,
-        dubaiCityImage: dubaiCityImage,
-        amsterdamCityImage: amsterdamCityImage,
-        sydneyCityImage: sydneyCityImage,
-        limaCityImage: limaCityImage,
-        rioCityImage: rioCityImage,
-        barcelonaCityImage: barcelonaCityImage,
-        milanCityImage: milanCityImage,
-        melbourneCityImage: melbourneCityImage,
-        athensCityImage: athensCityImage,
-        apiDataLoaded: true
-      })
-    
+    this.setState({
+      cities: citiesData,
+      apiDataLoaded: true
+    })
+
   }
 
   displayCity = () => {
 
   }
 
-//this.state.cities[1].data.results[0]
+  //this.state.cities[1].data.results[0]
   render() {
-   
+
     return (
       <div className="App">
         <Nav />
         <main>
           <Route exact path="/" render={(props) =>
             <LandingPage cities={this.state.cities} />} />
-          <Route path="/profile/:city" render={(props) =>
-          {
+          <Route path="/profile/:city" render={(props) => {
             return this.state.apiDataLoaded ?
-              <ProfilePage cityImage={this.state.londonCityImage} />
+              <ProfilePage cityImage={this.state.cities} />
               : null
           }} />
         </main>
