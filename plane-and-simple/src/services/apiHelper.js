383d5e2b203be89f539b1adcf14ea3800b664a8b
Tambini <tambini.c@gmail.com>
2019-12-18 11:58:04 -0500

    updated the api to filter down the data even more

--- plane-and-simple/src/services/apiHelper.js
+++ plane-and-simple/src/services/apiHelper.js
@@ -6,12 +6,6 @@ const apiToken = "7jwucw1j1gjrqjg8j67gbnu57i3zpz0u"
 const city = ""
 
 
-export const fetchData = async () => {
-  const data = await axios.get(`${url}id=${city}&account=${accountID}&token=${apiToken}&fields=all`);
-  return data
-}
-
-
 export const allCities = async () => {
 
   const cities = ["London", "Madrid", "Paris", "Dubai", "Amsterdam", "Sydney", "Lima", "Rio_de_Janeiro", "Barcelona", "Milan", "Melbourne", "Athens"]
@@ -22,8 +16,13 @@ export const allCities = async () => {
 
   // console.log(cityUrls)
 
-
-  const cityPromises = cityUrls.map(cityUrl => axios.get(cityUrl))
+  const cityPromises = cityUrls.map(cityUrl =>  axios.get(cityUrl).then(result => {
+    return {
+      name: result.data.results[0].id,
+      data: result.data.results[0],
+    }
+  })
+)
 
 
   const cityData = await Promise.all(cityPromises)
