55f9f0a3dbf6ace266ab33418396ec30f7b79ae7
Tambini <tambini.c@gmail.com>
2019-12-18 10:48:17 -0500

    updated profile page to have images

--- plane-and-simple/src/components/ProfilePage.js
+++ plane-and-simple/src/components/ProfilePage.js
@@ -6,7 +6,7 @@ export default function ProfilePage(props) {
   // componentDidMount = async () => {
   //   // const response = await fetchData()
   //   // console.log(response.data);
-  //   // let cityImage = response.;
+  //   // let cityImage = response.data.city.images[0].sizes.original.url;
   //   // let citySnippit = response.data.results[0].intro;
   //   // let cityPopulation = response.data.results[0].properties[0].value;
   //   // console.log(cityImage);
@@ -21,12 +21,13 @@ export default function ProfilePage(props) {
   //   // })
 
   // }
-
-  console.log(props)
+  // props.city.images[0].sizes.original.url
+  console.log(props.cityImage)
+  console.log("hello")
   return (
     <div className="profile-container">
       {props &&
-        <img src={props.city.images[0].sizes.original.url} alt="city-image" />
+        <img src={props.londonCityImage} alt="city-image" />
       }
       {/* <h1> {this.state.snippit}</h1>
       <h2>Population = {this.state.population}</h2> */}
@@ -35,10 +36,3 @@ export default function ProfilePage(props) {
 
   )
 }
-
-
-
-
-
-
-
