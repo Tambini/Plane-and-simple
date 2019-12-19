c934f66eb62524f02c34c19d04327721878d58a6
Tambini <tambini.c@gmail.com>
2019-12-19 00:00:57 -0500

    Updating content for profile page

--- plane-and-simple/src/components/ProfilePage.js
+++ plane-and-simple/src/components/ProfilePage.js
@@ -13,6 +13,13 @@ class ProfilePage extends Component {
       }
     }
 
+    // const weather = this.props.
+    //   displayWeather = () => {
+    //     for (let i = 0; i < this.props.cities.length; i++) {
+    //       return
+    //     }
+    //   }
+
     console.log(cityData)
 
     console.log(this.props.cities)
@@ -26,21 +33,30 @@ class ProfilePage extends Component {
           <h1> Name: {cityData.id} , {cityData.country_id}</h1>
           <h1>Population: {cityData.properties[0].value}</h1>
           <h1> Intro: {cityData.snippet} {cityData.intro} </h1>
-          <h1> Climate
-            Average-max(by month): Jan {cityData.climate.temperature.average_max.months[0]} C
-                                   Feb {cityData.climate.temperature.average_max.months[1]} C
-                                   March {cityData.climate.temperature.average_max.months[2]} Celsius
-                                   April {cityData.climate.temperature.average_max.months[3]} Celsius
-                                   May {cityData.climate.temperature.average_max.months[4]} Celsius
-                                   June {cityData.climate.temperature.average_max.months[5]} Celsius
-                                   July {cityData.climate.temperature.average_max.months[6]} Celsius
-                                   Aug {cityData.climate.temperature.average_max.months[7]} Celsius
-                                   Sept {cityData.climate.temperature.average_max.months[8]} Celsius
-                                   Oct {cityData.climate.temperature.average_max.months[9]} Celsius
-                                   Nov {cityData.climate.temperature.average_max.months[10]} Celsius
-                                   Dec {cityData.climate.temperature.average_max.months[11]} Celsius
-            </h1>
-          
+          <h2>Climate Average-max(by month )& Average-min (by month</h2>
+          <div className="weather-container" >
+
+            <div> Jan {cityData.climate.temperature.average_max.months[0]} C </div>
+            <div> Feb {cityData.climate.temperature.average_max.months[1]} C </div>
+            <div> March {cityData.climate.temperature.average_max.months[2]} C </div>
+            <div> April {cityData.climate.temperature.average_max.months[3]} C </div>
+            <div>  May {cityData.climate.temperature.average_max.months[4]} C </div>
+            <div> June {cityData.climate.temperature.average_max.months[5]} C </div>
+            <div>  July {cityData.climate.temperature.average_max.months[6]} C </div>
+            <div>  Aug {cityData.climate.temperature.average_max.months[7]} C </div>
+            <div>  Sept {cityData.climate.temperature.average_max.months[8]} C </div>
+            <div>  Oct {cityData.climate.temperature.average_max.months[9]} C </div>
+            <div>  Nov {cityData.climate.temperature.average_max.months[10]}C  </div>
+            <div>  Dec {cityData.climate.temperature.average_max.months[11]} C  </div>
+
+
+
+
+
+
+
+          </div>
+
           {/* <h1> Quick Tips:  </h1> */}
 
         </div>
@@ -48,7 +64,7 @@ class ProfilePage extends Component {
 
         {/* <h2>Population = {this.state.population}</h2> */}
 
-      </div>
+      </div >
 
     )
   }
