4a81e514beb0d6edf4dd139ea429961a1ecc4f8a
Tambini <tambini.c@gmail.com>
2019-12-20 00:41:09 -0500

    Updated the Nav

--- plane-and-simple/src/components/Nav.js
+++ plane-and-simple/src/components/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="Nav">
       <ul>
-        <li>Home</li>
-        <li>Contact </li>
+        <Link to={`/`}>
+          <li>Home</li>
+        </Link>
+        {/* <li>Contact </li> */}
+
       </ul>
     </div>
 
